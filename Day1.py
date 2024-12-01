de9c374997be1bc791eff179c8d60af9e4b2eeec
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2024-12-01 14:46:49 +0100

    Day 1 tasks

---
+++ Day1.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print(f"Your band name could be {city} {pet}")
+
+name = input("What is your name?\n")
+character = len(name)
+print(f"Length of your name {name} is {character} characters")
