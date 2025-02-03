8255af57b85a598ed8ee04feaf99dbd1433bebed
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-02-03 18:50:44 +0100

    Day 20 snake turning

--- Day 20 Snake Game/main.py
+++ Day 20 Snake Game/main.py
@@ -12,6 +12,12 @@ game_on = True
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 while game_on:
     screen.update() #Smooth turtle move
     time.sleep(0.1)
--- Day 20 Snake Game/snake.py
+++ Day 20 Snake Game/snake.py
@@ -2,6 +2,7 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+directions = [0, 90, 180, 270]
 
 class Snake:
 
@@ -15,9 +16,27 @@ class Snake:
             snake.goto(snk)
             self.segments.append(snake)
 
+        self.head = self.segments[0]
+
     def move(self):
-        for move in range(len(self.segments) - 1, 0, -1):
+        for move in range(len(self.segments) - 1, 0, -1): #Reverse order. From last to first (2, 1, 0)
             new_x = self.segments[move - 1].xcor()
             new_y = self.segments[move - 1].ycor()
             self.segments[move].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != directions[3]:
+            self.head.setheading(directions[1])
+
+    def down(self):
+        if self.head.heading() != directions[1]:
+            self.head.setheading(directions[3])
+
+    def left(self):
+        if self.head.heading() != directions[0]:
+            self.head.setheading(directions[2])
+
+    def right(self):
+        if self.head.heading() != directions[2]:
+            self.head.setheading(directions[0])
