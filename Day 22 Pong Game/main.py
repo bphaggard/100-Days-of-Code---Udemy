bb8cdb874c927b12ef47e04fc18379478f48d1fb
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-02-05 19:10:53 +0100

    Day 22 second paddle

--- Day 22 Pong Game/main.py
+++ Day 22 Pong Game/main.py
@@ -9,11 +9,14 @@ screen.tracer(0) #works with while loop
 
 game_on = True
 
-paddle = Paddle()
+right_paddle = Paddle((370, 0))
+left_paddle = Paddle((-380, 0))
 
 screen.listen()
-screen.onkey(paddle.move_up, "Up")
-screen.onkey(paddle.move_down, "Down")
+screen.onkey(right_paddle.move_up, "Up")
+screen.onkey(right_paddle.move_down, "Down")
+screen.onkey(left_paddle.move_up, "w")
+screen.onkey(left_paddle.move_down, "s")
 
 while game_on:
     screen.update()
--- Day 22 Pong Game/paddle.py
+++ Day 22 Pong Game/paddle.py
@@ -4,13 +4,13 @@ MOVE_DISTANCE = 20
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.penup()
         self.shapesize(stretch_len=1, stretch_wid=5)
         self.color("white")
-        self.goto(-380, 0)
+        self.goto(position)
 
     def move_up(self):
         new_y = self.ycor() + MOVE_DISTANCE
@@ -19,4 +19,3 @@ class Paddle(Turtle):
     def move_down(self):
         new_y = self.ycor() - MOVE_DISTANCE
         self.goto(self.xcor(), new_y)
-
