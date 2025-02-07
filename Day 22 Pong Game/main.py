97f30f194a24e91e2c081a12d04d94240a961e7b
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-02-07 14:48:35 +0100

    Day 22 ball bounce from the wall and the paddles

--- Day 22 Pong Game/main.py
+++ Day 22 Pong Game/main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.setup(800, 600)
@@ -11,6 +13,7 @@ game_on = True
 
 right_paddle = Paddle((370, 0))
 left_paddle = Paddle((-380, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(right_paddle.move_up, "Up")
@@ -19,6 +22,20 @@ screen.onkey(left_paddle.move_up, "w")
 screen.onkey(left_paddle.move_down, "s")
 
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.ball_move()
+
+    #Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() <= -280:
+        ball.wall_bounce()
+
+    #Detect collision with the right paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 340:
+        ball.paddle_bounce()
+
+    #Detect collision with the left paddle
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -350:
+        ball.paddle_bounce()
 
 screen.exitonclick()
