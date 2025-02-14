64573544381beb51d6991cd6d4c59ea40b8f244c
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-02-14 19:00:37 +0100

    Mail Merge

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,8 +1,8 @@
 #READ file
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("my_file.txt") as file: #Absolute file path "/Users/patrikmlcoch/Desktop/my_file.txt", Relative path "./my_file" for the current folder
+    contents = file.read()
+    print(contents)
 
 #WRITE to file
-with open("my_file.txt", mode="a") as file: # mode "w" rewrite everything, "a" will append
-    file.write("\nnew text")
+# with open("my_file.txt", mode="a") as file: # mode "w" rewrite everything, "a" will append
+#     file.write("\nnew text")
