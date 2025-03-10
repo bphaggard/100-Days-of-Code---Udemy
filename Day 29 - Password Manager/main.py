c8bdeb937c0eddf14b649b4f3347219dcb473f8e
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-03-10 11:53:59 +0100

    Saving data

--- Day 29 - Password Manager/main.py
+++ Day 29 - Password Manager/main.py
@@ -1,8 +1,21 @@
+import tkinter
 from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = entry_1.get()
+    email = entry_2.get()
+    password = entry_3.get()
 
+    with open("data.txt", mode="a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+
+    entry_1.delete(0, END)
+    entry_2.delete(0, END)
+    entry_3.delete(0, END)
+    entry_1.focus()
+    entry_2.insert(0, "example@gmail.com")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -20,6 +33,7 @@ label_1.grid(column=0, row=1)
 
 #Website entry
 entry_1 = Entry(width=38)
+entry_1.focus()
 entry_1.grid(column=1, row=1, columnspan=2)
 
 #Email/Username label
@@ -28,6 +42,7 @@ label_2.grid(column=0, row=2)
 
 #Email entry
 entry_2 = Entry(width=38)
+entry_2.insert(0, "example@gmail.com")
 entry_2.grid(column=1, row=2, columnspan=2)
 
 #Password label
@@ -42,7 +57,7 @@ entry_3.grid(column=1, row=3)
 gen_pass = Button(text="Generate Password")
 gen_pass.grid(column=2, row=3)
 
-add_pass = Button(text="Add", width=36)
+add_pass = Button(text="Add", width=36, command=save_password)
 add_pass.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
