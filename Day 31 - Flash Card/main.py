f08c97ac4a6f83835748586d831c2b456f00cf3c
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-03-18 17:37:21 +0100

    Day 31 Done

--- Day 31 - Flash Card/main.py
+++ Day 31 - Flash Card/main.py
@@ -3,14 +3,24 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-data = pandas.read_csv("/Users/patrikmlcoch/PycharmProjects/100DaysofCode/Day 31 - Flash Card/data/french_words.csv")
-data_dict = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv(
+        "/Users/patrikmlcoch/PycharmProjects/100DaysofCode/Day 31 - Flash Card/data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv(
+        "/Users/patrikmlcoch/PycharmProjects/100DaysofCode/Day 31 - Flash Card/data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records") #type list
+
 
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(data_dict)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card_image, image=card_front_image)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
@@ -20,6 +30,12 @@ def flip_card():
     canvas.itemconfig(card_image, image=card_back_image)
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("/Users/patrikmlcoch/PycharmProjects/100DaysofCode/Day 31 - Flash Card/data/words_to_learn.csv", index=False)
+    next_card()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -44,7 +60,7 @@ button_no = Button(image=no_image, highlightthickness=0, borderwidth=0, command=
 button_no.grid(column=0, row=1)
 
 yes_image = PhotoImage(file="/Users/patrikmlcoch/PycharmProjects/100DaysofCode/Day 31 - Flash Card/images/right.png")
-button_yes = Button(image=yes_image, highlightthickness=0, borderwidth=0, command=next_card)
+button_yes = Button(image=yes_image, highlightthickness=0, borderwidth=0, command=is_known)
 button_yes.grid(column=1, row=1)
 
 next_card()
