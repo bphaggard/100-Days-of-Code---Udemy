9cf9b96b6f60e704a7a656bfb2a8464de375e61c
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-04-07 21:39:42 +0200

    Advanced authentication with HTTP Header

--- Day 37 - Habit Tracking Project, API Post Requests & Headers/main.py
+++ Day 37 - Habit Tracking Project, API Post Requests & Headers/main.py
@@ -1,14 +1,34 @@
 import requests
 
+USERNAME = "patrick89"
+TOKEN = "kujdfg56er69"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "kujdfg56er69",
-    "username": "patrick89",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-# Create new account
-response = requests.post(url=pixela_endpoint, json=user_params)
+# # Create new account
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+# Create graph
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(response.text)
