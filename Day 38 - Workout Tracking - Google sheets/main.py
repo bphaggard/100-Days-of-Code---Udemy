b529ab7b48815240c2a6b7daecb027aa0fcd86cf
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-04-09 19:44:45 +0200

    Day 38 Step 2 Done

--- Day 38 - Workout Tracking - Google sheets/main.py
+++ Day 38 - Workout Tracking - Google sheets/main.py
@@ -1,5 +1,31 @@
 import os
 
+import requests
+
 APP_ID = os.environ.get("APP_ID_NUTRI")
 API_KEY = os.environ.get("API_KEY_NUTRI")
 
+GENDER = "male"
+WEIGHT_KG = 79
+HEIGHT_CM = 178
+AGE = 36
+
+user_query = input("Tell me which exercises you did: ")
+
+nutritionix_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+nutri_headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY
+}
+
+nutri_item = {
+    "query": user_query,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(url=nutritionix_endpoint, json=nutri_item, headers=nutri_headers)
+data = response.json()
+print(data)
