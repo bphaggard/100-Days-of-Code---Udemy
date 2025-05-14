a777661558bc6dbcb78bc0212e73706cec157189
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-05-14 17:43:47 +0200

    Day 51 - Reading down/up speed and login to platform X

--- Day 51 - X twitter Bot/InternetSpeedXBot.py
+++ Day 51 - X twitter Bot/InternetSpeedXBot.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 class InternetSpeedXBot:
-    def __init__(self, promised_down, promised_up):
+    def __init__(self, promised_down, promised_up, x_email, x_password):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_experimental_option("detach", True)
         self.driver = webdriver.Chrome(options=chrome_options)
@@ -12,6 +12,8 @@ class InternetSpeedXBot:
         self.up = promised_up
         self.download = 0.0
         self.upload = 0.0
+        self.email = x_email
+        self.password = x_password
 
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
@@ -50,5 +52,34 @@ class InternetSpeedXBot:
         except NoSuchElementException:
             print("No upload speed value found")
 
+    def login_to_x(self):
+        self.driver.get("https://x.com/")
+        time.sleep(2)
+
+        # Sign in button
+        try:
+            login_button = self.driver.find_element(By.LINK_TEXT, value="Sign in")
+            login_button.click()
+        except NoSuchElementException:
+            print("Sign in button not found.")
+            return
+
+        # Fill email
+        time.sleep(5)
+        login_email = self.driver.find_element(By.CSS_SELECTOR, 'input[name="text"][autocomplete="username"]')
+        login_email.send_keys(self.email)
+        time.sleep(1)
+        next_button = self.driver.find_element(By.XPATH, value='//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/button[2]/div')
+        next_button.click()
+
+        # Fill password
+        time.sleep(2)
+        login_password = self.driver.find_element(By.CSS_SELECTOR, 'input[name="password"][autocomplete="current-password"]')
+        login_password.send_keys(self.password)
+        time.sleep(1)
+        log_in_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/button/div/span/span')
+        log_in_button.click()
+
     def tweet_at_provider(self):
+        # message: Hey Internet Provider, why is my internet speed self.download/self.upload when I pay for 100/100?
         pass
--- Day 51 - X twitter Bot/main.py
+++ Day 51 - X twitter Bot/main.py
@@ -9,5 +9,6 @@ load_dotenv()
 X_EMAIL = os.getenv("X_EMAIL")
 X_PASSWORD = os.getenv("X_PASSWORD")
 
-speed_test = InternetSpeedXBot(PROMISED_DOWN, PROMISED_UP)
+speed_test = InternetSpeedXBot(PROMISED_DOWN, PROMISED_UP, X_EMAIL, X_PASSWORD)
 speed_test.get_internet_speed()
+speed_test.login_to_x()
