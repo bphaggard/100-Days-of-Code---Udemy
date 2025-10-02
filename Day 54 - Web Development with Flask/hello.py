e52f426870dfc911ef25eacb4b78f68d0f7b8ce9
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-02 17:37:09 +0200

    Day 52 changes

--- Day 54 - Web Development with Flask/hello.py
+++ Day 54 - Web Development with Flask/hello.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+if __name__ == "__main__":  # with this code you can use PyCharm Run and Stop button instead of flask run command
+    app.run()
