f142ecb00f9cdff6b9d31ba469576c688ab2840a
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-02 17:40:26 +0200

    Day 52 changes, start Day 55

---
+++ Day 55 - Flask URL parsing/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/bye")
+def bye():
+    return "Bye"
+
+if __name__ == "__main__":  # with this code you can use PyCharm Run and Stop button instead of flask run command
+    app.run()
