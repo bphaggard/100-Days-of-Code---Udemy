2025babe81bff4b8e862c30980640f471959a38b
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-09 16:47:15 +0200

    Multiple statements with jinja

--- Day 57 - Templating with Jinja in Flask/server.py
+++ Day 57 - Templating with Jinja in Flask/server.py
@@ -1,6 +1,7 @@
 import datetime
 import random
 
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,6 +11,13 @@ def home_page():
     random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
+
+@app.route("/blog")
+def blog():
+    response_blog = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response_blog.raise_for_status()
+    blog_data = response_blog.json()
+    return render_template("blog.html", posts=blog_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
