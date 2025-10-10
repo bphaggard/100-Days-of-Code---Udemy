296f2003dca9795ece052f074643f2b4c98370a1
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-10 18:14:44 +0200

    Day 57 - Final Project Done

--- Day 57 - Templating with Jinja in Flask/Final Project/main.py
+++ Day 57 - Templating with Jinja in Flask/Final Project/main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 
 
@@ -5,7 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response_blog = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response_blog.raise_for_status()
+    blog_data = response_blog.json()
+    return render_template("index.html", posts=blog_data)
+
+@app.route("/post/<int:post_id>")
+def post_page(post_id):
+    response_blog = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    response_blog.raise_for_status()
+    blog_data = response_blog.json()
+    post = next((item for item in blog_data if item['id'] == post_id), None)
+    if post:
+        return render_template("post.html", posts=post)
+    else:
+        return "Post not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
