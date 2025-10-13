8d8b2a4b160db188d7e18158cdeafe9cd6768991
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-13 20:44:03 +0200

    Day 59 Done - post.html

--- Day 59 - Upgraded Blog from Day 57/main.py
+++ Day 59 - Upgraded Blog from Day 57/main.py
@@ -1,10 +1,14 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    response_blog = requests.get("https://api.npoint.io/8a2a9787f98be1677ea5")
+    response_blog.raise_for_status()
+    blog_data = response_blog.json()
+    return render_template("index.html", posts=blog_data)
 
 @app.route("/about")
 def about_page():
@@ -13,6 +17,17 @@ def about_page():
 @app.route("/contact")
 def contact_page():
     return render_template("contact.html")
+
+@app.route("/post/<int:post_id>")
+def post_page(post_id):
+    response_blog = requests.get("https://api.npoint.io/8a2a9787f98be1677ea5")
+    response_blog.raise_for_status()
+    blog_data = response_blog.json()
+    post = next((item for item in blog_data if item['id'] == post_id), None)
+    if post:
+        return render_template("post.html", posts=post)
+    else:
+        return "Post not found"
 
 if __name__ == "__main__":
     app.run(debug=True)
