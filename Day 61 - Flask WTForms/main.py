817db4f8d0fc7553de2859b2c0da32ee306c70bb
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-15 21:32:24 +0200

    Day 61 Flask WTForms

---
+++ Day 61 - Flask WTForms/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from wtforms.validators import DataRequired
+import os
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+
+class MyForm(FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), validators.Email()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Log In')
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    log_form = MyForm()
+    log_form.validate_on_submit()
+    return render_template('login.html', form=log_form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
