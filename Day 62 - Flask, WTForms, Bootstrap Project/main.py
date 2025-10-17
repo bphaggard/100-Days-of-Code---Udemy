ba5dd6f6bbec6936f62c657f18564c4d2a163bde
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-17 19:24:42 +0200

    Day 62 Done

--- Day 62 - Flask, WTForms, Bootstrap Project/main.py
+++ Day 62 - Flask, WTForms, Bootstrap Project/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.choices import SelectField
+from wtforms.validators import DataRequired, URL
 import csv
 
 '''
@@ -25,16 +26,19 @@ Bootstrap5(app)
 
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
+    location = StringField('Cafe Location on Google Maps (URL)', validators=[DataRequired(), URL()])
+    opening = StringField('Opening Time e.g. 8AM', validators=[DataRequired()])
+    closing = StringField('Closing Time e.g. 5:30PM', validators=[DataRequired()])
+    rating = SelectField('Coffee Rating',
+                         choices=[('0', '✘'), ('1', '☕️'), ('2', '☕️☕️'), ('3', '☕️☕️☕️'), ('4', '☕️☕️☕️☕️'), ('5', '☕️☕️☕️☕️☕️')],
+                         validators=[DataRequired()])
+    wifi = SelectField('Wifi Strength Rating',
+                       choices=[('0','✘'), ('1', '💪'), ('2', '💪💪'), ('3', '💪💪💪'), ('4', '💪💪💪💪'), ('5', '💪💪💪💪💪')],
+                       validators=[DataRequired()])
+    power = SelectField('Power Socket Availability',
+                        choices=[('0', '✘'), ('1', '🔌'), ('2', '🔌🔌'), ('3', '🔌🔌🔌'), ('4', '🔌🔌🔌🔌'), ('5', '🔌🔌🔌🔌🔌')],
+                        validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
 
 # all Flask routes below
 @app.route("/")
@@ -42,14 +46,30 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        try:
+            with open('cafe-data.csv', 'rb+') as f:
+                f.seek(-1, 2)
+                if f.read(1) != b'\n':
+                    f.write(b'\n')
+        except OSError:
+            pass
+
+        with open('cafe-data.csv', mode='a', newline='', encoding='utf-8') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([
+                form.cafe.data,
+                form.location.data,
+                form.opening.data,
+                form.closing.data,
+                form.rating.data,
+                form.wifi.data,
+                form.power.data
+            ])
+        return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
