b20c3fd205d84a61c316c6fb557995360f325db4
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-21 18:58:35 +0200

    saving books from list to SQL db, edit and delete

--- Day 63 - Databases SQLite and SQLAlchemy/main.py
+++ Day 63 - Databases SQLite and SQLAlchemy/main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -16,28 +19,63 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db.init_app(app)
 
-all_books = []
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
-class BookForm(FlaskForm):
-    name = StringField('Book Name', validators=[DataRequired()])
-    author = StringField('Book Author', validators=[DataRequired()])
-    rating = StringField('Book Rating', validators=[DataRequired()])
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
+    with app.app_context():
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = result.scalars().all()
     return render_template("index.html", books=all_books)
+
+@app.route('/edit/<int:book_id>', methods=['GET', 'POST'])
+def edit_rating(book_id):
+    with app.app_context():
+        book_to_update = db.get_or_404(Book, book_id)
+        if request.method == "POST":
+            new_rating = request.form["new_rating"]
+            book_to_update.rating = float(new_rating)
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template("edit.html", book=book_to_update)
+
+@app.route('/delete/<int:book_id>')
+def delete_book(book_id):
+    with app.app_context():
+        book_to_delete = db.get_or_404(Book, book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
+    return redirect(url_for("home"))
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == "POST":
-        all_books.append({
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        })
-        return render_template("index.html", books=all_books)
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+        return render_template("index.html")
     return render_template("add.html")
 
 if __name__ == "__main__":
