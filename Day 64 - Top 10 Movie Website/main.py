3d8eb8ecd4cb28d12da8aed68474bb19a0fb3382
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2025-10-22 18:56:10 +0200

    Created DB, Form for add.html

--- Day 64 - Top 10 Movie Website/main.py
+++ Day 64 - Top 10 Movie Website/main.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField, TextAreaField, FloatField
+from wtforms.validators import DataRequired, NumberRange, URL
 import requests
 
 '''
@@ -25,16 +25,59 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+class MovieForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    year = IntegerField("Year", validators=[DataRequired(), NumberRange(min=1880, max=2100)])
+    description = TextAreaField("Description", validators=[DataRequired()])
+    rating = FloatField("Rating", validators=[DataRequired(), NumberRange(min=0, max=10)])
+    ranking = IntegerField("Ranking", validators=[DataRequired()])
+    review = TextAreaField("Review", validators=[DataRequired()])
+    img_url = StringField("Image URL", validators=[DataRequired(), URL()])
+    submit = SubmitField("Add Movie")
+
 # CREATE DB
+class Base(DeclarativeBase):
+  pass
 
+db = SQLAlchemy(model_class=Base)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-collection.db"
+db.init_app(app)
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 # CREATE TABLE
-
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    form = MovieForm()
+    if form.validate_on_submit():
+        new_movie = Movie(
+            title=form.title.data,
+            year=form.year.data,
+            description=form.description.data,
+            rating=form.rating.data,
+            ranking=form.ranking.data,
+            review=form.review.data,
+            img_url=form.img_url.data
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('add.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
